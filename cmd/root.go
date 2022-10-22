530898c122fc9f8a1d02993044f03ef2dae5c3ad
Ben Lechlitner <otherland@gmail.com>
2022-10-21 22:01:58 -0400

    use word/phrase generation package

--- cmd/root.go
+++ cmd/root.go
@@ -7,14 +7,16 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
+
+	"github.com/milligan22963/passphrase/pkg/ppgen"
 )
 
 var (
+	rootCmd *cobra.Command
+
 	// Used for flags
-	dict        string
 	separator   string
 	phraseCount int
 )
@@ -25,36 +27,14 @@ const (
 )
 
 const rootCommandLongDesc string = "passphrase is a password generator for " +
-	"multi-word passphrases based on an XKCD comic (936). The list of seed words " +
-	"can be customized by the user and length of password modified as needed."
-
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:               "passphrase [flags]",
-	Version:           "1.0.0",
-	Short:             "An app to generate a random passphrase",
-	Long:              rootCommandLongDesc,
-	Example:           `passphrase --separator='-' --number=4 --dictionary="./wordlist.txt"`,
-	Args:              cobra.NoArgs,
-	PersistentPreRunE: validateFlags,
-	RunE:              RunRootCmd,
-}
+	"multi-word passphrases based on an XKCD comic (936)."
 
 func init() {
-	// global params/flags
-	rootCmd.PersistentFlags().StringVarP(&dict, "dictionary", "d", "wordlist.txt", "Path to word source (text file).")
-
-	// main app params/flags
-	rootCmd.Flags().IntVarP(&phraseCount, "number", "n", MinimumWordCount, "Number of words to include.")
-	rootCmd.Flags().StringVarP(&separator, "separator", "s", "_", "Separator between words.")
+	NewRootCmd()
 }
 
 // ValidateFlags checks that the flags are within expected boundaries.
 func validateFlags(cmd *cobra.Command, args []string) error {
-	if _, err := os.Stat(dict); err != nil {
-		return fmt.Errorf("bad word list: %w", err)
-	}
-
 	if phraseCount < MinimumWordCount {
 		return fmt.Errorf("invalid number of words: number = %d", phraseCount)
 	}
@@ -62,15 +42,38 @@ func validateFlags(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-// GetRootCmd gets the application root command.
-func GetRootCmd() *cobra.Command {
+// NewRootCmd creates the application root command.
+func NewRootCmd() *cobra.Command {
+	// rootCmd represents the base command when called without any subcommands
+	rootCmd = &cobra.Command{
+		Use:     "passphrase [flags]",
+		Version: "1.0.0",
+		Short:   "An app to generate a random passphrase",
+		Long:    rootCommandLongDesc,
+		Example: `passphrase --separator='-' --number=4`,
+		Args:    cobra.NoArgs,
+		PreRunE: validateFlags,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			out, err := ppgen.GeneratePassPhrase(phraseCount, separator)
+			if err != nil {
+				return fmt.Errorf("failed to generate passphrase")
+			}
+
+			cmd.Println(out)
+			return nil
+		},
+	}
+
+	// main app params/flags
+	rootCmd.Flags().IntVarP(&phraseCount, "number", "n", MinimumWordCount, "Number of words to include.")
+	rootCmd.Flags().StringVarP(&separator, "separator", "s", "_", "Separator between words.")
+
 	return rootCmd
 }
 
-// RunRootCmd is executed when the application is run without any subcommands.
-func RunRootCmd(cmd *cobra.Command, args []string) error {
-	cmd.Printf("passphrase called: dictionary=%s; number=%d; separator=%s\n", dict, phraseCount, separator)
-	return nil
+// GetRootCmd gets the application root command.
+func GetRootCmd() *cobra.Command {
+	return rootCmd
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
