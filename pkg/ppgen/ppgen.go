ca296a5ce4d1a2fedf2bc9eb835dcd1f5290b02d
Ben Lechlitner <otherland@gmail.com>
2022-10-24 00:22:05 -0400

    add seed parameter to passphrase generation
    switching to "math/rand" to be able to set seed value
    will use pseudorandom seed from runtime if seed not specified

--- pkg/ppgen/ppgen.go
+++ pkg/ppgen/ppgen.go
@@ -6,38 +6,63 @@
 package ppgen
 
 import (
-	"crypto/rand"
 	"fmt"
-	"math/big"
+	"hash/fnv"
+	"math/rand"
 	"strings"
+	"time"
 
 	"github.com/tyler-smith/go-bip39/wordlists"
 )
 
 // GeneratePhraseWords picks 'n' number of words at random. Function is deterministic for repeat uses of the same seed.
-func GeneratePhraseWords(n int) ([]string, error) {
+func GeneratePhraseWords(n int, seed string) ([]string, error) {
+	if n < 1 {
+		return nil, fmt.Errorf("number of words must be greater than 0, n = %d", n)
+	}
+
 	wordList := wordlists.English
 	max := len(wordList)
 	var words []string
 
-	// Initialize pseudorandom with time if no value is provided.
-	for i := 0; i < n; i++ {
-		wordIndex, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
+	if len(seed) > 0 {
+		seedHash, err := hash(seed)
 		if err != nil {
-			return nil, fmt.Errorf("failed to get word %d of %d: %w", i+1, n, err)
+			return nil, fmt.Errorf("failed to hash seed: %w", err)
 		}
 
-		words = append(words, wordList[wordIndex.Int64()])
+		rand.Seed(seedHash)
+	} else {
+		rand.Seed(time.Now().UnixNano())
+	}
+
+	// Initialize pseudorandom with time if no value is provided.
+	for i := 0; i < n; i++ {
+		wordIndex := rand.Intn(max) //nolint:gosec // we can probably ignore this as a security issue
+
+		words = append(words, wordList[wordIndex])
 	}
 
 	return words, nil
 }
 
 // GeneratePassPhrase creates a string with `n` words, separated by `separator`.
-func GeneratePassPhrase(n int, separator string) (string, error) {
-	words, err := GeneratePhraseWords(n)
+func GeneratePassPhrase(n int, separator string, seed string) (string, error) {
+	words, err := GeneratePhraseWords(n, seed)
 	if err != nil {
-		return "", fmt.Errorf("failed to generate passphrase with %d words, separator=\"%s\": %w", n, separator, err)
+		return "", err
 	}
+
 	return strings.Join(words, separator), nil
+}
+
+func hash(s string) (int64, error) {
+	h := fnv.New64a()
+
+	_, err := h.Write([]byte(s))
+	if err != nil {
+		return 0, err
+	}
+
+	return int64(h.Sum64()), nil
 }
