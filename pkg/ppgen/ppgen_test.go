8de58073f73429f496b37f8f6a9d59ccd276ca39
Ben Lechlitner <otherland@gmail.com>
2022-10-24 23:41:01 -0400

    add more tests to passphrase package

--- pkg/ppgen/ppgen_test.go
+++ pkg/ppgen/ppgen_test.go
@@ -25,37 +25,46 @@ func TestGeneratePhraseWords(t *testing.T) {
 		want      []string
 		assertion assert.ErrorAssertionFunc
 	}{
-		{
-			name: "expected default",
-			args: args{
-				n:    4,
-				seed: "1",
-			},
-			want:      []string{"alcohol", "lucky", "draw", "ghost"},
-			assertion: assert.NoError,
-		},
-		{
-			name: "basic with custom seed",
-			args: args{
-				n:    4,
-				seed: "cat",
-			},
-			want:      []string{"sudden", "dentist", "clerk", "practice"},
-			assertion: assert.NoError,
-		},
-		{
-			name: "negative number of words",
-			args: args{
-				n:    -1,
-				seed: "",
-			},
-			want:      []string{},
-			assertion: assert.Error,
-		},
+		{"expected default", args{4, "1"}, []string{"alcohol", "lucky", "draw", "ghost"}, assert.NoError},
+		{"basic with custom seed", args{4, "cat"}, []string{"sudden", "dentist", "clerk", "practice"}, assert.NoError},
+		{"negative number of words", args{-1, ""}, []string{}, assert.Error},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := ppgen.GeneratePhraseWords(tt.args.n, tt.args.seed)
+			tt.assertion(t, err)
+
+			if err == nil {
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
+
+func TestGeneratePhrase(t *testing.T) {
+	type args struct {
+		n    int
+		s    string
+		seed string
+	}
+
+	tests := []struct {
+		name      string
+		args      args
+		want      string
+		assertion assert.ErrorAssertionFunc
+	}{
+		{"expected default", args{4, "_", "1"}, "alcohol_lucky_draw_ghost", assert.NoError},
+		{"basic with custom seed", args{4, "_", "cat"}, "sudden_dentist_clerk_practice", assert.NoError},
+		{"negative number of words", args{-1, "_", ""}, "", assert.Error},
+		{"zero words", args{0, "_", ""}, "", assert.Error},
+		{"empty separator", args{4, "", "cat"}, "suddendentistclerkpractice", assert.NoError},
+		{"numeric separator", args{4, "3", "cat"}, "sudden3dentist3clerk3practice", assert.NoError},
+		{"emoji separator", args{4, "🐈", "cat"}, "sudden🐈dentist🐈clerk🐈practice", assert.NoError},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ppgen.GeneratePassPhrase(tt.args.n, tt.args.s, tt.args.seed)
 			tt.assertion(t, err)
 
 			if err == nil {
